1baf74b004c08d10151c3244e0b0cead47627454
Diego Carranza <carranza.diego.1a@gmail.com>
2023-11-28 17:53:04 -0500

    Cambios realizados en Huffman.py

--- huffman.py
+++ huffman.py
@@ -7,7 +7,7 @@ from collections import defaultdict, Counter
 class FuenteDeInformacion:
     def generar_mensaje(self):
         mensaje = {
-            "mensaje": "Puros corridos tumbados"
+            "mensaje" : "Corridos Tumbados"
         }
         return json.dumps(mensaje)
 
@@ -43,50 +43,48 @@ class NodoHuffman:
     def __lt__(self, otro):
         return self.frecuencia < otro.frecuencia
     
-#Codificación Huffman
+class Huffman:
+    def construir_arbol_huffman(self, datos):
+        frecuencias = Counter(datos)
+        cola_prioridad = [NodoHuffman(caracter, frecuencia) for caracter, frecuencia in frecuencias.items()]
+        heapq.heapify(cola_prioridad)
 
-def construir_arbol_huffman(datos):
-    frecuencias = Counter(datos)
-    cola_prioridad = [NodoHuffman(caracter, frecuencia) for caracter, frecuencia in frecuencias.items()]
-    heapq.heapify(cola_prioridad)
+        while len(cola_prioridad) > 1:
+            izquierda = heapq.heappop(cola_prioridad)
+            derecha = heapq.heappop(cola_prioridad)
+            nodo_intermedio = NodoHuffman(None, izquierda.frecuencia + derecha.frecuencia)
+            nodo_intermedio.izquierda = izquierda
+            nodo_intermedio.derecha = derecha
+            heapq.heappush(cola_prioridad, nodo_intermedio)
 
-    while len(cola_prioridad) > 1:
-        izquierda = heapq.heappop(cola_prioridad)
-        derecha = heapq.heappop(cola_prioridad)
-        nodo_intermedio = NodoHuffman(None, izquierda.frecuencia + derecha.frecuencia)
-        nodo_intermedio.izquierda = izquierda
-        nodo_intermedio.derecha = derecha
-        heapq.heappush(cola_prioridad, nodo_intermedio)
+        return cola_prioridad[0]
 
-    return cola_prioridad[0]
+    def generar_codificacion_huffman(self, arbol_huffman, prefijo="", codificacion={}):
+        if arbol_huffman.caracter is not None:
+            codificacion[arbol_huffman.caracter] = prefijo
+        if arbol_huffman.izquierda is not None:
+            self.generar_codificacion_huffman(arbol_huffman.izquierda, prefijo + "0", codificacion)
+        if arbol_huffman.derecha is not None:
+            self.generar_codificacion_huffman(arbol_huffman.derecha, prefijo + "1", codificacion)
 
-def generar_codificacion_huffman(arbol_huffman, prefijo="", codificacion={}):
-    if arbol_huffman.caracter is not None:
-        codificacion[arbol_huffman.caracter] = prefijo
-    if arbol_huffman.izquierda is not None:
-        generar_codificacion_huffman(arbol_huffman.izquierda, prefijo + "0", codificacion)
-    if arbol_huffman.derecha is not None:
-        generar_codificacion_huffman(arbol_huffman.derecha, prefijo + "1", codificacion)
+    def codificar_mensaje(self, mensaje, codificacion):
+        mensaje_codificado = ""
+        for caracter in mensaje:
+            mensaje_codificado += codificacion[caracter]
+        return mensaje_codificado
 
-def codificar_mensaje(mensaje, codificacion):
-    mensaje_codificado = ""
-    for caracter in mensaje:
-        mensaje_codificado += codificacion[caracter]
-    return mensaje_codificado
-
-def decodificar_mensaje(mensaje_codificado, arbol_huffman):
-    mensaje_decodificado = ""
-    nodo_actual = arbol_huffman
-    for bit in mensaje_codificado:
-        if bit == "0":
-            nodo_actual = nodo_actual.izquierda
-        else:
-            nodo_actual = nodo_actual.derecha
-        if nodo_actual.caracter is not None:
-            mensaje_decodificado += nodo_actual.caracter
-            nodo_actual = arbol_huffman
-    return mensaje_decodificado
-
+    def decodificar_mensaje(self, mensaje_codificado, arbol_huffman):
+        mensaje_decodificado = ""
+        nodo_actual = arbol_huffman
+        for bit in mensaje_codificado:
+            if bit == "0":
+                nodo_actual = nodo_actual.izquierda
+            else:
+                nodo_actual = nodo_actual.derecha
+            if nodo_actual.caracter is not None:
+                mensaje_decodificado += nodo_actual.caracter
+                nodo_actual = arbol_huffman
+        return mensaje_decodificado
 
 if __name__ == "__main__":
     fuente = FuenteDeInformacion()
@@ -114,19 +112,12 @@ if __name__ == "__main__":
     # Mostrar el mensaje en el destino (en binario)
     destino.mostrar_mensaje(mensaje_recibido)
 
-        # Construir el árbol de Huffman
-    arbol_huffman = construir_arbol_huffman(mensaje_original)
-    
-    # Generar la codificación Huffman
-    codificacion = {}
-    generar_codificacion_huffman(arbol_huffman, codificacion=codificacion)
-    
-    # Codificar el mensaje original
-    mensaje_codificado = codificar_mensaje(mensaje_original, codificacion)
-    
-    # Decodificar el mensaje codificado
-    mensaje_decodificado = decodificar_mensaje(mensaje_codificado, arbol_huffman)
-    
-    print("Mensaje Original:", mensaje_original)
-    print("Mensaje Codificado:", mensaje_codificado)
-    print("Mensaje Decodificado:", mensaje_decodificado)
+huffman = Huffman()
+arbol_huffman = huffman.construir_arbol_huffman(mensaje_original)
+codificacion = {}
+huffman.generar_codificacion_huffman(arbol_huffman, "", codificacion)
+mensaje_codificado = huffman.codificar_mensaje("mensaje", codificacion)
+mensaje_decodificado = huffman.decodificar_mensaje(mensaje_codificado, arbol_huffman)
+
+print("Mensaje codificado:", mensaje_codificado)
+print("Mensaje decodificado:", mensaje_decodificado)
